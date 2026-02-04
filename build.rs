c8ad9d66eb1f901da1345c4116110b9ea3514292
hachimamma <subhodisha2062@gmail.com>
2026-02-04 23:37:40 +0530

    UPDATE: Updated workflows

--- build.rs
+++ build.rs
@@ -16,8 +16,19 @@ fn main() {
         .flag("-Wall")
         .flag("-Wextra");
 
-    build.include("/usr/include");
-    build.include("/usr/local/include");
+    let possible_include_paths = [
+        "/usr/include",
+        "/usr/local/include",
+        "/usr/include/x86_64-linux-gnu",
+        "/usr/lib/x86_64-linux-gnu/include",
+    ];
+
+    for path in &possible_include_paths {
+        if std::path::Path::new(path).exists() {
+            build.include(path);
+            println!("cargo:warning=Adding include path: {path}");
+        }
+    }
 
     build.compile("vault_engine");
 
