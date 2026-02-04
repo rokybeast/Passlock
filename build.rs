1d8cbc7bc6dc3952b3ce8c261d67cb7340cc73be
hachimamma <subhodisha2062@gmail.com>
2026-02-04 23:25:53 +0530

    UPDATE: Updated workflows

--- build.rs
+++ build.rs
@@ -8,13 +8,18 @@ fn main() {
     println!("cargo:rerun-if-changed={}/vault_engine.c", c_src.display());
     println!("cargo:rerun-if-changed={}/vault_engine.h", c_src.display());
 
-    cc::Build::new()
+    let mut build = cc::Build::new();
+    build
         .file(c_src.join("vault_engine.c"))
         .include(&c_src)
         .opt_level(3)
         .flag("-Wall")
-        .flag("-Wextra")
-        .compile("vault_engine");
+        .flag("-Wextra");
+
+    build.include("/usr/include");
+    build.include("/usr/local/include");
+
+    build.compile("vault_engine");
 
     println!("cargo:rustc-link-lib=sodium");
 
@@ -28,5 +33,6 @@ fn main() {
     {
         println!("cargo:rustc-link-search=/usr/lib");
         println!("cargo:rustc-link-search=/usr/local/lib");
+        println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu");
     }
 }
