70493679d97160927eff0efb50f8844d43c0c275
hachimamma <subhodisha2062@gmail.com>
2026-02-08 18:52:48 +0530

    EDIT: Edited mod.rs

--- src/ui/widgets/mod.rs
+++ src/ui/widgets/mod.rs
@@ -3,7 +3,6 @@ pub mod passwords;
 pub mod utility;
 pub mod vault;
 
-// Re-export commonly used functions
 pub use menu::draw_main_menu;
 pub use passwords::{draw_add_pwd, draw_del_pwd, draw_edit_pwd, draw_history, draw_view_pwds};
 pub use utility::{draw_filter_tags, draw_gen_pwd, draw_search_pwd};
